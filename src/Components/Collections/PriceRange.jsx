0008fd405e639f27d3e4c79a967084fcdfc3dab8
Dorisa Shehi <doris.shehi2@gmail.com>
2024-07-04 11:08:21 +0200

    Price Range sider added to filter

---
+++ src/Components/Collections/PriceRange.jsx
@@ -0,0 +1,44 @@
+import MultiRangeSlider from "multi-range-slider-react";
+import { useState } from "react";
+const PriceRange = () => {
+  const [minValue, set_minValue] = useState(25);
+  const [maxValue, set_maxValue] = useState(75);
+  const handleInput = (e) => {
+    set_minValue(e.minValue);
+    set_maxValue(e.maxValue);
+  };
+
+  return (
+    <>
+      <div className="filter-content mb-5">
+        ${minValue}.00 - ${maxValue}.00
+      </div>
+
+      <MultiRangeSlider
+        className="price-slider"
+        style={{
+          border: "none",
+          boxShadow: "none",
+          padding: "15px 10px",
+        }}
+        min={0}
+        max={100}
+        step={5}
+        minValue={minValue}
+        maxValue={maxValue}
+        barLeftColor="var(--border-color)"
+        barRightColor="var(--border-color)"
+        barInnerColor="var(--primary-color)"
+        thumbLeftColor="var(--primary-color)"
+        thumbRightColor="var(--primary-color)"
+        ruler="false"
+        label="false"
+        onInput={(e) => {
+          handleInput(e);
+        }}
+      />
+    </>
+  );
+};
+
+export default PriceRange;
