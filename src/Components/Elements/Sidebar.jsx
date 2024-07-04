d360f031b69c3d5c31e6647c99077ccfd01c4f06
Dorisa Shehi <doris.shehi2@gmail.com>
2024-07-04 21:07:56 +0200

    Open close Sidebar functionality added

--- src/Components/Elements/Sidebar.jsx
+++ src/Components/Elements/Sidebar.jsx
@@ -1,8 +1,16 @@
-const Sidebar = ({ children }) => {
+const Sidebar = ({ sidebarState, children }) => {
   return (
     <>
-      <div className="fixed top-0 bg-backdrop left-0 w-[100%] z-40 p-5 h-[100%] flex flex-col md:hidden"></div>
-      <div className="fixed top-0 bg-white left-0 w-[80%] z-50 p-5 h-[100%] flex flex-col md:hidden">
+      <div
+        className={`fixed top-0 bg-backdrop left-0 w-[100%] z-40 p-5 h-[100%] transition ease-out duration-75 ${
+          sidebarState ? "flex" : "hidden"
+        } flex-col`}
+      ></div>
+      <div
+        className={`fixed top-0 bg-white left-0 w-[80%] z-50 p-5 h-[100%] transition ease-out duration-75 ${
+          sidebarState ? "flex" : "hidden"
+        } flex-col`}
+      >
         {children}
       </div>
     </>
