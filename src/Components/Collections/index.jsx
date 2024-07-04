5a972094390f62ba345d59084bc789e98dc9d33a
Dorisa Shehi <doris.shehi2@gmail.com>
2024-07-04 17:33:41 +0200

    Sort by select added

--- src/Components/Collections/index.jsx
+++ src/Components/Collections/index.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import Banner from "../Banner";
 import FilterComponent from "./FilterComponent";
 import ImageCard from "../Elements/ImageCard";
 import exampleImg from "../../assets/images/cart/example-cloth.png";
+import Select from "react-tailwindcss-select";
 const Collections = () => {
+  const options = [
+    { value: "Asc", label: "Ascending" },
+    { value: "Desc", label: "Descending" },
+  ];
+  const [sorted, setSorted] = useState(null);
+
+  const handleChange = (value) => {
+    console.log("value:", value);
+    setSorted(value);
+  };
   return (
     <>
       <Banner title="Shop" path="Home / Collections" />
@@ -14,9 +26,29 @@ const Collections = () => {
             </div>
 
             <div className="flex-1">
-              <div className="flex justify-between mb-10">
+              <div className="flex justify-between mb-10 items-center">
                 <div className="text-primary">Showing 1-12 of 240 results</div>
-                <div className="text-primary">Sort by</div>
+                <div className="text-primary flex w-[300px] items-center">
+                  <span className="flex-none mr-3">Sort by</span>
+                  <Select
+                    primaryColor="var(--primary-color)"
+                    value={sorted}
+                    placeholder="Defult Sorting"
+                    onChange={handleChange}
+                    options={options}
+                    classNames={{
+                      menuButton: () =>
+                        `flex text-sm cursor-pointer border-2 border border-borderColor rounded-[0.2em] outline-0 transition-all duration-300 focus:outline-0`,
+                      menu: "absolute z-10 w-full bg-white shadow-sm font-normal border-2 border border-borderColor rounded-[0.2em] py-1 mt-1.5 text-sm text-primary cursor-pointer",
+                      listItem: ({ isSelected }) =>
+                        `block transition duration-200 px-2 py-2 cursor-pointer select-none truncate rounded ${
+                          isSelected
+                            ? `text-primary`
+                            : `text-primary hover:bg-yellow opacity-[0.8]`
+                        }`,
+                    }}
+                  />
+                </div>
               </div>
               <div className="flex gap-5 items-center mb-10">
                 <h1 className="text-primary">Active Filters</h1>
