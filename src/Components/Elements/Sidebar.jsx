53a9d4dd5de09832d350a13bf8dd48cf16a92ef7
Dorisa Shehi <doris.shehi2@gmail.com>
2024-07-05 08:52:28 +0200

    Filter by separated in another component

--- src/Components/Elements/Sidebar.jsx
+++ src/Components/Elements/Sidebar.jsx
@@ -2,13 +2,13 @@ const Sidebar = ({ sidebarState, children }) => {
   return (
     <>
       <div
-        className={`fixed top-0 bg-backdrop left-0 w-[100%] z-40 p-5 h-[100%] transition ease-out duration-75 ${
+        className={`fixed top-0 bg-backdrop left-0 w-[100%] z-40 p-5 h-[100%] ${
           sidebarState ? "flex" : "hidden"
         } flex-col`}
       ></div>
       <div
-        className={`fixed top-0 bg-white left-0 w-[80%] z-50 p-5 h-[100%] transition ease-out duration-75 ${
-          sidebarState ? "flex" : "hidden"
+        className={`fixed top-0 bg-white left-0 w-[80%] z-50 p-5 h-[100%] duration-500 flex transition-all ease-out  ${
+          sidebarState ? "ml-0" : "ml-[-100%]"
         } flex-col`}
       >
         {children}
