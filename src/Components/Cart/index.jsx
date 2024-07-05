ed071762c3428e0bd99b9efa80d74c2f00c4d95d
Dorisa Shehi <doris.shehi2@gmail.com>
2024-07-05 20:41:33 +0200

    Shopping cart made mobile responsive

--- src/Components/Cart/index.jsx
+++ src/Components/Cart/index.jsx
@@ -9,21 +9,30 @@ const Cart = () => {
         <div className="content">
           <div className="flex gap-10 flex-col lg:flex-row">
             <div className="flex-1">
-              <div class="relative overflow-x-auto">
-                <table class="w-full text-sm text-left text-primary cursor-pointer">
-                  <thead class="text-xs text-primary uppercase bg-yellow">
+              <div className="relative overflow-x-auto">
+                <table className="w-full text-sm text-left text-primary cursor-pointer">
+                  <thead className="text-xs text-primary uppercase bg-yellow">
                     <tr>
                       <th></th>
-                      <th scope="col" className="px-6 py-3 hidden md:block">
+                      <th
+                        scope="col"
+                        className="px-6 py-3 hidden md:table-cell"
+                      >
                         Product
                       </th>
                       <th scope="col" className="px-6 py-3">
                         Name
                       </th>
-                      <th scope="col" className="px-6 py-3  hidden md:block">
+                      <th
+                        scope="col"
+                        className="px-6 py-3  hidden md:table-cell"
+                      >
                         Price
                       </th>
-                      <th scope="col" className="px-6 py-3  hidden md:block">
+                      <th
+                        scope="col"
+                        className="px-6 py-3  hidden md:table-cell"
+                      >
                         Quantity
                       </th>
                       <th scope="col" className="px-6 py-3">
@@ -32,7 +41,7 @@ const Cart = () => {
                     </tr>
                   </thead>
                   <tbody>
-                    <tr class="bg-white border-b border-borderColor text-primary">
+                    <tr className="bg-white border-b border-borderColor text-primary">
                       <td className="">
                         <svg
                           className="filter-icons cursor-pointer"
@@ -51,22 +60,22 @@ const Cart = () => {
                           ></path>
                         </svg>
                       </td>
-                      <td className="hidden md:block">
+                      <td className="hidden md:table-cell">
                         <img
                           src={exampleImg}
                           className="w-[100px] h-[100px] object-cover"
                         />
                       </td>
-                      <td class="px-6 py-4">Hat</td>
-                      <td class="px-6 py-4 hidden md:block">$56</td>
-                      <td class="px-6 py-4 hidden md:block">2</td>
-                      <td class="px-6 py-4">$294</td>
+                      <td className="px-6 py-4">Hat</td>
+                      <td className="px-6 py-4 hidden md:table-cell">$56</td>
+                      <td className="px-6 py-4 hidden md:table-cell">2</td>
+                      <td className="px-6 py-4">$294</td>
                     </tr>
                   </tbody>
                 </table>
               </div>
             </div>
-            <div className="w-[350px] pr-5 lg:block border-2 border-borderColor px-7 py-5 h-fit">
+            <div className="w-full md:w-[350px] pr-5 lg:block border-2 border-borderColor px-7 py-5 h-fit">
               <div className="border-b-2 border-borderColor pb-3 font-medium">
                 <h1 className="">Cart</h1>
               </div>
@@ -84,7 +93,7 @@ const Cart = () => {
                   <span className="text-primary font-medium">$00.00</span>
                 </li>
               </ul>
-              <div className=" w-[100%] bg-white pb-10 pt-5 xl:pb-6 lg:pb-40">
+              <div className=" w-[100%] bg-white mt-3">
                 <Button
                   classN="cart-btn justify-center text-[0.9rem] md:text-sm"
                   text="Proceed To Checkout"
