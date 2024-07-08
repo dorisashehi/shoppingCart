09569af88f33bfe14f32f3f2913c091618a32d4c
Dorisa Shehi <doris.shehi2@gmail.com>
2024-07-08 09:18:22 +0200

    Problem with categories key fixed

--- src/Components/Collections/FilterOptions.jsx
+++ src/Components/Collections/FilterOptions.jsx
@@ -1,6 +1,39 @@
+import { useEffect, useState } from "react";
 import CostumCheckbox from "../Elements/CostumCheckbox";
 import PriceRange from "./PriceRange";
+
 const FilterOptions = () => {
+  const [filters, setFilters] = useState({
+    categories: [],
+  });
+
+  const fetchCategories = async () => {
+    //FETCH SPECIFIC CATEGORIES AVAILABLE
+    const categoryListUrl = "https://dummyjson.com/products/category-list";
+    const response = await fetch(categoryListUrl); //GET ALL WOMEN CATEGORIES FROM LIST OF CATEGORIES
+    if (!response.ok) {
+      throw new Error("Couldn't fetch data");
+    }
+    const catArray = await response.json();
+
+    const womenCategories = catArray.filter(
+      (
+        category //FILTER ONES THAT ARE RELATED TPO WOMENS
+      ) => category.startsWith("womens-")
+    );
+
+    return womenCategories;
+  };
+
+  useEffect(() => {
+    const getCat = async () => {
+      const cat = await fetchCategories();
+      setFilters(cat);
+    };
+
+    getCat();
+  }, []);
+
   return (
     <>
       <div className="filter-section">
@@ -10,12 +43,13 @@ const FilterOptions = () => {
       <div className="filter-section">
         <h1 className="filter-header">Category</h1>
         <ul className="filter-content">
-          <li className="filter-item">
-            <CostumCheckbox label="Man" />
-          </li>
-          <li className="filter-item">
-            <CostumCheckbox label="Women" />
-          </li>
+          {Object.values(filters).map((cat, index) => {
+            return (
+              <li className="filter-item" key={index}>
+                <CostumCheckbox label={cat} />
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className="filter-section">
