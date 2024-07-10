cbf6de4f3da695764fa02e6c625a96fd4249c477
Dorisa Shehi <doris.shehi2@gmail.com>
2024-07-10 15:05:40 +0200

    Plus minus added to each product

--- src/Components/Elements/ImageCard.jsx
+++ src/Components/Elements/ImageCard.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 const ImageCard = ({ imgSrc, category, lowPrice, proName, price, classN }) => {
+  const [addedToCard, setAdded] = useState(false);
+
+  const addToCard = () => {
+    console.log("hello");
+    setAdded(true);
+  };
   return (
     <div className={`card ${classN}`}>
       <div className="relative">
@@ -23,23 +31,51 @@ const ImageCard = ({ imgSrc, category, lowPrice, proName, price, classN }) => {
               d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
             ></path>
           </svg>
+        </div>
 
-          <svg
-            className="slider-icon"
-            data-slot="icon"
-            fill="none"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-            ></path>
-          </svg>
+        <div className="icons absolute p-3  bottom-0 left-0 flex flex-row w-full justify-center text-center opacity-[0.8] gap-3">
+          <div className="flex flex-row gap-5 items-center">
+            {/* <input type="button" value="-" />
+            <input type="text" value="" />
+            <input type="button" value="+" /> */}
+
+            <svg
+              data-slot="icon"
+              className="slider-icon"
+              fill="none"
+              stroke-width="1.5"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M5 12h14"
+              ></path>
+            </svg>
+
+            <span className="slider-icon">1</span>
+
+            <svg
+              data-slot="icon"
+              className="slider-icon"
+              onClick={addToCard}
+              fill="none"
+              stroke-width="1.5"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              ></path>
+            </svg>
+          </div>
         </div>
       </div>
       <div className="content py-5">
