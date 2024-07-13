9dded036c441adea3a88897c16eeb4c58e1e9a30
Dorisa Shehi <doris.shehi2@gmail.com>
2024-07-13 16:06:27 +0200

    UseReducers implemented to card state

--- src/Context/CartProvider.jsx
+++ src/Context/CartProvider.jsx
@@ -1,54 +1,77 @@
 import { CartContext } from "./CartContext";
-import { useState } from "react";
+import { useReducer, useState } from "react";
 
 const CartProvider = ({ children }) => {
-  const [card, setCard] = useState([]); //CART STATE
+  //const [card, setCard] = useState([]); //CART STATE
 
-  const findPro = (proID) => {
-    return card.findIndex((item) => item.id === proID); //FIND INDEX OF THE PRODUCT IN CART
+  const initialCard = [];
+
+  const findPro = (proID, cardItems) => {
+    return cardItems.findIndex((item) => item.id === proID); //FIND INDEX OF THE PRODUCT IN CART
   };
 
-  const addToCard = (newProd) => {
-    // Check if the item with the same id already exists in the cart
-    const index = findPro(newProd.id); //FIND INDEX OF THE PRODUCT IN CART
+  const reducer = (card, action) => {
+    if (action.type === "increment") {
+      // Check if the item with the same id already exists in the cart
+      const index = findPro(action.newProd.id, card); //FIND INDEX OF THE PRODUCT IN CART
 
-    if (index !== -1) {
-      // If id exists, update the quantity
-      const updatedCard = [...card];
-      updatedCard[index].quantity += 1;
-      setCard(updatedCard);
-    } else {
-      //NOT IN CARD ADD IT
-      setCard([...card, newProd]);
+      if (index !== -1) {
+        // If id exists, update the quantity
+        const updatedCard = [...card];
+        updatedCard[index].quantity += 1;
+        return updatedCard;
+        //setCard(updatedCard);
+      } else {
+        //NOT IN CARD ADD IT
+
+        return [...card, action.newProd];
+        // setCard([...card, newProd]);
+      }
     }
+
+    if (action.type === "remove") {
+      const index = findPro(updatedProID, card);
+      if (index !== -1) {
+        //IF ITS PRESENT
+        const updatedCart = [...card];
+        updatedCart[index].quantity -= 1; //DISCOUNT QUANTITY BY 1
+
+        if (updatedCart[index].quantity === 0) {
+          //WHEN THE QUANTITY OF THE PRODUCT REACHES 0, REMOVE IT FROM CART
+          updatedCart.splice(index, 1);
+        }
+
+        return updatedCart;
+      }
+    }
+
+    if (action.type === "deleteAdded") {
+      //DELETE PROJECT FROM CART SIDEBAR
+
+      const index = findPro(action.proID, card);
+
+      //IF ITS PRESENT
+      const updatedCart = [...card];
+      //DELETE PRODUCT FROM CART
+      updatedCart.splice(index, 1);
+
+      return updatedCart;
+    }
+    return card; //NOT TYPE, JUST RETURN ITS INITIAL VALUE
+  };
+  const [card, dispatch] = useReducer(reducer, initialCard);
+
+  const addToCard = (newProd) => {
+    dispatch({ type: "increment", newProd: newProd });
   };
 
   const removeFromCart = (updatedProID) => {
-    const index = findPro(updatedProID);
-    if (index !== -1) {
-      //IF ITS PRESENT
-      const updatedCart = [...card];
-      updatedCart[index].quantity -= 1; //DISCOUNT QUANTITY BY 1
-
-      if (updatedCart[index].quantity === 0) {
-        //WHEN THE QUANTITY OF THE PRODUCT REACHES 0, REMOVE IT FROM CART
-        updatedCart.splice(index, 1);
-      }
-      setCard(updatedCart);
-    }
+    dispatch({ type: "remove", payload: updatedProID });
   };
 
   const deleteAddedPro = (proID) => {
     //DELETE PROJECT FROM CART SIDEBAR
-
-    const index = findPro(proID);
-
-    //IF ITS PRESENT
-    const updatedCart = [...card];
-    //DELETE PRODUCT FROM CART
-    updatedCart.splice(index, 1);
-
-    setCard(updatedCart);
+    dispatch({ type: "deleteAdded", payload: proID });
   };
 
   const calculateRealPrice = (price, percentage) => {
@@ -71,9 +94,9 @@ const CartProvider = ({ children }) => {
             calculateRealPrice(product.price, product.discountPercentage)
         );
       }
+
       return total + curr.quantity * product.price; //PRODUCT HASN'T DISCOUNT
     }, 0);
-
     return Number.parseFloat(total).toFixed(2);
   };
 
